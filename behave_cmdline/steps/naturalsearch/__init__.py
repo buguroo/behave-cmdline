302072a892292d8026143ae4942b7158a968c2c9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-05-11 18:51:59 +0200

    Use substep in naturalsearch.

--- behave_cmdline/steps/naturalsearch/__init__.py
+++ behave_cmdline/steps/naturalsearch/__init__.py
@@ -1,11 +1,8 @@
 from . import _steps
 from ..stepcollection.stepcollection import define_steps
-from ..stepcollection.substeps import SubSteps
 from .i18n import languages
-
-substeps = SubSteps()
 
 define_steps(r"^behave_cmdline\.steps\.naturalsearch\.(?P<lang>[^\.]+)$",
              _steps,
              languages,
-             substeps.step)
+             substeps=True)
