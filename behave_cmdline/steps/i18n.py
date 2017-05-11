6b5b04c0260b05d42d49b1d05d2be5a24c6574a9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-05-11 19:32:01 +0200

    Using type integer

--- behave_cmdline/steps/i18n.py
+++ behave_cmdline/steps/i18n.py
@@ -15,7 +15,7 @@ languages = {
              'within a maximium of "{timeout:d}" seconds')],
         'in_the_output_of_happens_that': [
             'in the output "{stream}" of "{alias}" happens that',
-            'in the output "{stream}" of "{alias}", in less than "{timeout}" seconds, happens that'
+            'in the output "{stream}" of "{alias}", in less than "{timeout:d}" seconds, happens that'
         ]
     },
     'es': {
@@ -37,7 +37,7 @@ languages = {
              'como máximo en "{timeout:d}" segundos')],
         'in_the_output_of_happens_that': [
             'en la salida "{stream}" de "{alias}" ocurre que',
-            'en la salida "{stream}" de "{alias}", en menos de "{timeout}" segundos, ocurre que'
+            'en la salida "{stream}" de "{alias}", en menos de "{timeout:d}" segundos, ocurre que'
         ]
     },
 }
