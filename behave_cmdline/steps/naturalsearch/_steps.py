d9e5de66a14ced230942b4e22a63a0e76eaa051f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-05-11 19:24:58 +0200

    Missing imports

--- behave_cmdline/steps/naturalsearch/_steps.py
+++ behave_cmdline/steps/naturalsearch/_steps.py
@@ -21,24 +21,28 @@ def _check_lines(do_match, shows):
 
 
 def i_see_the_string(context, string):
+    from functools import partial
     do_match = partial(_plain_matcher, string)
     check = _check_lines(do_match, shows=True)
     yield from check
 
 
 def i_dont_see_the_string(context, string):
+    from functools import partial
     do_match = partial(_plain_matcher, string)
     check = _check_lines(do_match, shows=False)
     yield from check
 
 
 def the_regex_matches(context, regex):
+    from functools import partial
     do_match = partial(_regex_matcher, regex)
     check = _check_lines(do_match, shows=True)
     yield from check
 
 
 def the_regex_doesnt_match(context, regex):
+    from functools import partial
     do_match = partial(_regex_matcher, regex)
     check = _check_lines(do_match, shows=False)
     yield from check
