3a4373f65a39167c3495daa4229e768ce39f206c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-05-23 12:01:01 +0200

    Fix

--- behave_cmdline/steps/naturalsearch/_steps.py
+++ behave_cmdline/steps/naturalsearch/_steps.py
@@ -23,26 +23,26 @@ def _check_lines(do_match, shows, row):
 def i_see_the_string(context, string):
     from functools import partial
     do_match = partial(_plain_matcher, string)
-    check = _check_lines(do_match, shows=True, string)
+    check = _check_lines(do_match, shows=True, row=string)
     yield from check
 
 
 def i_dont_see_the_string(context, string):
     from functools import partial
     do_match = partial(_plain_matcher, string)
-    check = _check_lines(do_match, shows=False, string)
+    check = _check_lines(do_match, shows=False, row=string)
     yield from check
 
 
 def the_regex_matches(context, regex):
     from functools import partial
     do_match = partial(_regex_matcher, regex)
-    check = _check_lines(do_match, shows=True, regex)
+    check = _check_lines(do_match, shows=True, row=regex)
     yield from check
 
 
 def the_regex_doesnt_match(context, regex):
     from functools import partial
     do_match = partial(_regex_matcher, regex)
-    check = _check_lines(do_match, shows=False, regex)
+    check = _check_lines(do_match, shows=False, row=regex)
     yield from check
